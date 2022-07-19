ccef86d6a52caa1b2c22ba98f55f68f0f4611ef8
RAZIQ YUSRI <106171654+RY9901@users.noreply.github.com>
2022-07-20 01:16:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,22 @@
-const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors');
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = process.env.PORT || 5000
 
-const app = express();
-app.use(cors());
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
-var connect = mysql.createPool({
-  host: "ec2-176-34-215-248.eu-west-1.compute.amazonaws.com",
-  user: "zaiaryvqbpwwcb",
-  password: "731fafeb016f84ea7f87300cbd19a24ba3e96adbaaf92504bc8d945d0302489b",
-  database: "delu1t92658u0"
-});
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
 
-app.get('/', (req, res) => {
-  res.send('hello world!');
-  console.log('Running');
-});
+app.get('/users', db.getUsers)
 
-app.get('/displayallparticipant', (req, res) => {
-  var micro_username = req.query.username;
-
-  console.log("username: " + micro_username);
-
-  connect.getConnection(function (err, connection) {
-    if (err) { res.send('Error Database Connection'); }
-    else {
-      var sql = "select participant_id, participant_password,participant_name,participant_email,participant_phoneno";
-      connect.query(sql, function (err, result) {
-        if (err) { throw err; }
-        else {
-          res.send(result);
-        }
-      connection.release();
-      });
-    }
-  });
-});
-
-
-app.listen(process.env.PORT, () => {
-  console.log('Example app listening to port 4005');
-});
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
