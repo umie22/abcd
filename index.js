63bd86bb9b6afd18f532f51ecb041a2b42aca538
RAZIQ YUSRI <106171654+RY9901@users.noreply.github.com>
2022-07-20 10:10:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const db = require('./queries')
+const { Pool } = require('pg');
+const express = require('express');
+const cors = require('cors');
+const path = require('path')
 
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
 
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+const app = express();
+app.use(cors());
+const pool = new Pool({
+  host: "ec2-176-34-215-248.eu-west-1.compute.amazonaws.com",
+  user: "zaiaryvqbpwwcb",
+  password: "731fafeb016f84ea7f87300cbd19a24ba3e96adbaaf92504bc8d945d0302489b",
+  database: "delu1t92658u0"
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
 
-app.get('/getParticipants', db.getParticipants)
+pool.on('error', (err, client) => {
+    console.error('Error:', err);
+});
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
+
+app.get('/', (req, res) => {
+  res.send('Masjid');
+  console.log('Running');
+});
+
+
+const query = `
+SELECT *
+FROM participant
+`;
+
+
+app.get('/display', (req, res) => {
+  var micro_username = req.query.username;
+
+  console.log("username: " + micro_username);
+
+  pool.connect(function (err, client) {
+    if (err) { res.send('Error Database Connection'); }
+    else {
+      client.query(query, function (err, result) {
+        if (err) { throw err; }
+        else {
+          res.send(result);
+        }
+      connection.release();
+      });
+    }
+  });
+});
+
+app.listen(process.env.PORT ,() => {
+  console.log('app listening ');
+});
